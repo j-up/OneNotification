5d857e774ffdb9706cc096c17a427099fa122c8c
JM <ad4674@gmail.com>
2020-05-03 16:22:59 +0900

    update

--- app/src/main/java/com/jup/oneNotification/view/MainActivity.kt
+++ app/src/main/java/com/jup/oneNotification/view/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.jup.oneNotification.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.TimePickerDialog.OnTimeSetListener
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
 import com.jup.oneNotification.R
 import com.jup.oneNotification.databinding.ActivityMainBinding
+import com.jup.oneNotification.utils.JLog
 import com.jup.oneNotification.viewModel.MainViewModel
 
 
@@ -15,6 +19,18 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val mainViewModel = MainViewModel()
 
+        val timeDialog = DialogFragment()
+
+        val callbackMethod =
+            OnTimeSetListener { view, hourOfDay, minute ->
+                //setText(hourOfDay.toString() + "시" + minute + "분")
+            }
+
+
+
+        mainViewModel.onTimeSetting.observe(this, Observer {
+            JLog.d(this,"s")
+        })
 
         binding.mainViewModel=mainViewModel
         binding.lifecycleOwner=this@MainActivity
--- app/src/main/java/com/jup/oneNotification/viewModel/MainViewModel.kt
+++ app/src/main/java/com/jup/oneNotification/viewModel/MainViewModel.kt
@@ -1,7 +1,15 @@
 package com.jup.oneNotification.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+    private val _onTimeSetting = MutableLiveData<Boolean>()
 
+    val onTimeSetting: LiveData<Boolean> get () = _onTimeSetting
+
+    fun onTimeSetting() {
+        _onTimeSetting.value = true
+    }
 }
