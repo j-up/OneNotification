95913dbc0b0b8edcd61fb8ea1fa712ec0f3f8524
JM <ad4674@gmail.com>
2020-07-27 22:51:24 +0900

    openWeather DI 적용

--- app/src/main/java/com/jup/oneNotification/core/di/module/MyModule.kt
+++ app/src/main/java/com/jup/oneNotification/core/di/module/MyModule.kt
@@ -3,25 +3,37 @@ package com.jup.oneNotification.core.di.module
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import com.jup.oneNotification.core.network.OpenWeather
 import com.jup.oneNotification.core.provider.LocationProvider
 import com.jup.oneNotification.core.service.LocationWorker
-import com.jup.oneNotification.utils.RequestPermission
+import com.jup.oneNotification.utils.PermissionUtil
 import com.jup.oneNotification.view.dialog.TimePickerFragment
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 private const val PREFERENCES_FILE_KEY = "com.jup.onenotification"
-
+private const val OPENWEATHER_BASE_URL = "http://api.openweathermap.org/"
 val appModule = module{
     single { provideSettingsPreferences(androidApplication()) }
 
     single { TimePickerFragment.newInstance(null,get()) }
 
-    single { RequestPermission(it[0])}
+    single { PermissionUtil(it[0])}
 
     single { LocationWorker(androidApplication()) }
     single { LocationProvider(get()) }
+    single { createOpenWeather() }
 }
 
 private fun provideSettingsPreferences(app: Application): SharedPreferences =
     app.getSharedPreferences(PREFERENCES_FILE_KEY, Context.MODE_PRIVATE)
+
+private fun createOpenWeather(): OpenWeather {
+    return Retrofit.Builder()
+        .baseUrl(OPENWEATHER_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(OpenWeather::class.java)
+}
--- app/src/main/java/com/jup/oneNotification/core/network/OpenWeather.kt
+++ app/src/main/java/com/jup/oneNotification/core/network/OpenWeather.kt
@@ -1,4 +1,11 @@
 package com.jup.oneNotification.core.network
 
+import com.jup.oneNotification.model.WeatherResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface OpenWeather {
+    @GET("data/2.5/weather?")
+    fun getCurrentWeatherData(@Query("lat") lat: String, @Query("lon") lon: String, @Query("APPID") app_id: String): Call<WeatherResponse>
 }
