34f386d32bd0106019d341a3d7b31eeb16f38ce5
JM <ad4674@gmail.com>
2020-07-27 22:52:02 +0900

    openWeather Response 추가

---
+++ app/src/main/java/com/jup/oneNotification/model/WeatherModel.kt
@@ -0,0 +1,31 @@
+package com.jup.oneNotification.model
+
+import com.google.gson.annotations.SerializedName
+
+class WeatherResponse {
+
+    @SerializedName("sys")
+    var sys: Sys? = null
+
+    @SerializedName("main")
+    var main: Main? = null
+}
+
+
+class Main {
+    @SerializedName("temp")
+    var temp: Float = 0.0f
+    @SerializedName("humidity")
+    var humidity: Float = 0.0f
+    @SerializedName("pressure")
+    var pressure: Float = 0.0f
+    @SerializedName("temp_min")
+    var temp_min: Float = 0.0f
+    @SerializedName("temp_max")
+    var temp_max: Float = 0.0f
+}
+
+class Sys {
+    @SerializedName("country")
+    var country: String? = null
+}
