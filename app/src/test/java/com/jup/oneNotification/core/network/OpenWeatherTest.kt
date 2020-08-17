5b9e8f23dedea494eca66a35d9d542b1c2bcf3d9
JM <ad4674@gmail.com>
2020-08-17 16:29:56 +0900

    AndroidTest -> JavaTest 변경 종속성 추가

--- app/src/test/java/com/jup/oneNotification/core/network/OpenWeatherTest.kt
+++ app/src/test/java/com/jup/oneNotification/core/network/OpenWeatherTest.kt
@@ -1,8 +1,5 @@
 package com.jup.oneNotification.core.network
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.jup.oneNotification.BuildConfig
 import com.jup.oneNotification.model.WeatherResponse
 import com.jup.oneNotification.utils.JLog
@@ -15,16 +12,16 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.text.SimpleDateFormat
 import java.util.*
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(RobolectricTestRunner::class)
 class OpenWeatherTest {
 
-    private val context = ApplicationProvider.getApplicationContext<Context>()
     private val testDispatcher = TestCoroutineDispatcher()
     private val retrofit: OpenWeather by lazy {
         Retrofit.Builder()
@@ -43,7 +40,12 @@ class OpenWeatherTest {
     fun getWeatherTest() {
         CoroutineScope(testDispatcher).launch {
             val weatherResponse = getWeather(retrofit,testDispatcher)
+            Assert.assertEquals("code:${weatherResponse.code()} message:${weatherResponse.message()}"
+                ,weatherResponse.isSuccessful,true)
+
+
             JLog.d(this::class.java,weatherResponse.body().toString())
+            println(weatherResponse.body().toString())
         }
     }
 
@@ -52,6 +54,7 @@ class OpenWeatherTest {
         val unixTimeArray = arrayListOf(1597633200,1597806000,1597892400,1597978800,1598065200,1598151600,1598238000)
         unixTimeArray.map {
             JLog.d(this::class.java,getUnixTimeToDate(it))
+            println(getUnixTimeToDate(it))
         }
     }
 
@@ -65,9 +68,6 @@ class OpenWeatherTest {
         val weatherResponse = retrofit
             .getCurrentWeatherData("65","135","minutely",BuildConfig.OpenWeatherKey)
             .execute()
-
-        Assert.assertEquals("code:${weatherResponse.code()} message:${weatherResponse.message()}"
-            ,weatherResponse.isSuccessful,true)
 
         weatherResponse
         }
