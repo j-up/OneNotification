64b6f178f7df199970c0ebff74abbebf5fc4eb5c
JM <ad4674@gmail.com>
2021-01-31 22:43:27 +0900

    application 이름 변경

--- app/src/main/java/com/jup/oneNotification/NotificationApplication.kt
+++ app/src/main/java/com/jup/oneNotification/NotificationApplication.kt
@@ -5,13 +5,13 @@ import org.koin.android.ext.koin.androidContext
 import com.jup.oneNotification.core.di.module.appModule
 import org.koin.core.context.startKoin
 
-class MyApplication: Application() {
+class NotificationApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
             printLogger()
-            androidContext(this@MyApplication)
+            androidContext(this@NotificationApplication)
             modules(appModule)
         }
     }
